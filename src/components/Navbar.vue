06ddd1c148a8713696863e71afc06956d51c6c95
Tomás Aprile <tomas.eaprile@gmail.com>
2019-10-01 19:16:17 -0300

    Navbar component

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="navbar" class="bg-harlequin inset-x-0 bottom-0 h-12 fixed container flex">
+    <navbar-tab to="/home" icon="home"/>
+    <navbar-tab to="/matches" icon="futbol"/>
+    <navbar-tab to="/charts" icon="chart-bar"/>
+    <navbar-tab to="/locations" icon="map-marked-alt"/>
+  </div>
+</template>
+
+<script>
+import NavbarTab from '@/components/NavbarTab.vue'
+
+export default {
+  name: 'TheNavbar',
+  components: {
+    NavbarTab
+  }
+}
+</script>
+
+<style scoped>
+  #navbar {
+    border-radius: 1.25rem 1.25rem 0 0;
+  }
+  
+</style>
---
+++ src/components/NavbarTab.vue
@@ -0,0 +1,20 @@
+<template>
+  <router-link
+      class="flex-1 flex justify-center items-center outline-none opacity-50"
+      :to="to">
+      <v-icon :name="icon" scale="1.45"/>
+    </router-link> 
+</template>
+
+<script>
+export default {
+  name: 'NavbarTab',
+  props: ['to', 'icon']
+}
+</script>
+
+<style>
+  .router-link-active {
+    @apply opacity-100;
+  }
+</style>
