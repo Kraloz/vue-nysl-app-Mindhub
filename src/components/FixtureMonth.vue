fbd609e7b592f06dc0b0b191885d26b9852cedcc
Tomás Aprile <tomas.eaprile@gmail.com>
2019-10-02 20:59:06 -0300

    *incomplete* Matches View

---
+++ src/components/FixtureMonth.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <ul class="w-full mt-4 text-2xl ml-20 w-full flex flex-col">
+      <li
+        v-for="(month, i) in months" :key="i"
+        class=" w-full h-full flex-1 my-2 border-solid border-1 border-gray-600">
+        <router-link to="/home"
+          class="h-full w-full block ml-4 py-4">
+          {{ month }}</router-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FixtureMonth',
+  data () {
+    return {
+      'months': ['October', 'November', 'December', 'January']
+    }
+  }
+}
+</script>
+
+<style>
+ul > li {
+    border-bottom: 1px solid black;
+  }
+</style>
