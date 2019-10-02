a38e7d4b968d22a358ecb0cbef7bdcc910d7dda1
Tomás Aprile <tomas.eaprile@gmail.com>
2019-10-01 21:56:26 -0300

    extend tailwing

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
         'harlequin': '#52eb00',
         'mercury': '#e2e2e2',
         'bayoux': '#386374'
+      },
+      fontFamily: {
+        display: ['Rubik', 'sans-serif']
+      },
+      borderRadius: {
+        'half': '50%'
+      },
+      spacing: {
+        '14': '3.5rem'
       }
     }
   }
