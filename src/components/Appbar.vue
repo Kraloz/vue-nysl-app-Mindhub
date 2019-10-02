08071712d2bd371b8782e8bc3cbcf962a7fea294
Tomás Aprile <tomas.eaprile@gmail.com>
2019-10-01 21:56:58 -0300

    finished maqueta layout

---
+++ src/components/Appbar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="header" class="bg-bayoux fixed inset-x-0 top-0 h-20 container flex text-white">
+    <div class="flex w-full">
+      <div class="flex-grow flex justify-center items-center">
+        <h1 class="font-display text-3xl">Home NYSL</h1>
+      </div>
+      <div class="flex-1 flex justify-center items-center">
+        <img class="w-14 h-14 rounded-half object-cover" src="@/assets/images/doge.jpeg" alt="Profile image">
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TheAppbar'
+}
+</script>
