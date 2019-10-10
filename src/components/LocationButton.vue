3cf2ab47daa695b3ddffcddbd372800bd8a3fa47
Tomás Aprile <tomas.aprile@fnetsystem.com.ar>
2019-10-10 00:04:46 -0300

    wip drawer

---
+++ src/components/LocationButton.vue
@@ -0,0 +1,13 @@
+<template>
+  <button class="flex py-4 flex-col border border-b border-black rounded-lg text-left">
+    <h5 class="ml-6 text-lg">{{ location.name }}</h5>
+      <h6 class="ml-10 text-xs">{{ location.address }}</h6>
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'LocationButton',
+  props: ['location']
+}
+</script>
